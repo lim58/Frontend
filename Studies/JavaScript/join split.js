9a3f8de35a830ed17fbe81c21a31547e87dbd58b
lim58 <dylim7715@gmail.com>
2023-09-30 19:45:45 +0900

    create : join split

---
+++ Studies/JavaScript/join split.js
@@ -0,0 +1,10 @@
+const arr = ["안녕", "하이", "반가워"]
+const user = "Mike,Jane,Ven"
+
+//배열 -> 문자열
+console.log(arr.join()) 
+console.log(arr.join("-"))
+
+console.log(user.split())
+console.log(user.split(""))
+console.log(user.split(","))
